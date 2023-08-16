a8a0f172d7aeb07b92115993754f6e48c728489a
jsaavedra <jose.saavedra>
2023-08-16 19:01:16 -0400

    encoder

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
         
     ds_valid= (
         ds_valid.shuffle(1024, seed=config_model.getint('SEED'))
-        .map(lambda x: map_func(x, lambda image:  tf.image.grayscale_to_rgb(image), n_classes = config_data.getint('N_CLASSES')), num_parallel_calls=AUTO)
+        .map(lambda x: map_func(x, lambda image:  tf.cast(tf.image.grayscale_to_rgb(image), tf.float32), n_classes = config_data.getint('N_CLASSES')), num_parallel_calls=AUTO)
         .batch(config_model.getint('BATCH_SIZE')))
                
     #----------------------------------------------------------------------------------
