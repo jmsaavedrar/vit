179609664eafc2ee07fa0216a63aa6bc343311d4
jsaavedra <jose.saavedra>
2023-08-16 21:12:34 -0400

    encoder

--- train.py
+++ train.py
@@ -66,8 +66,8 @@ if __name__ == '__main__':
     ds_train = (
         ds_train.shuffle(1024, seed=config_model.getint('SEED'))
         .map(lambda x: map_func(x,  daug.get_augmentation_fun(), n_classes = config_data.getint('N_CLASSES')), num_parallel_calls=AUTO)
-        .batch(config_model.getint('BATCH_SIZE')))
-        #.prefetch(AUTO) )
+        .batch(config_model.getint('BATCH_SIZE'))
+        .prefetch(AUTO))
     
         
     ds_valid= (
