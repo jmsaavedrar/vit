4952777cad0419c745cebf707bc9cad8d8200a92
jose.saavedra <jose.saavedra>
2023-08-22 11:12:16 -0400

    ckp

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ if __name__ == '__main__':
             #               metrics=['accuracy'])
             model_file = os.path.join(model_dir, 'model', 'model')
             if args.only_test :                
-                model.load_weights(model_file)
+                model.load_weights(config_model.get('CKP_FILE'))
                 print('--- weights loaded from {} ---'.format(model_file))
                 model.evaluate(ds_valid, steps = n_steps_valid)
                 
